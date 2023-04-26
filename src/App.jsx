0462f35b6d9d941ada265ac2d58d5e397e042cd8
john <johnsantosh2@gmail.com>
2023-04-26 20:42:13 +0530

    para-generator completd build

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,43 @@
+import { useEffect, useState } from "react";
+// library for unique id like uuid
+import { nanoid } from "nanoid";
+import data from "./data";
 const App = () => {
-  return <h2>Lorem Ipsum Starter</h2>;
+  const [text, setText] = useState([]);
+  const [count, setCount] = useState(0);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    const result = data.slice(0, count);
+    setText(result);
+  }
+
+  return (
+    <main>
+      <div className="container">
+        <h2>TIRED OF BORING LOREM IPSUM?</h2>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="number">Paragraphs : </label>
+          <input
+            id="number"
+            type="number"
+            value={count}
+            placeholder="0 - 8 "
+            min={1}
+            step="1"
+            max={data.length}
+            onChange={(e) => {
+              setCount(e.target.value);
+            }}
+          />
+          <button>Generate</button>
+        </form>
+        {text.map((para) => (
+          <p key={nanoid()}>{para}</p>
+        ))}
+      </div>
+    </main>
+  );
 };
 export default App;
